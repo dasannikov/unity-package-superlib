cac1289e57fe1ab4f2011c7cd54a1d3b227b2b6b
Dmitry Sannikov <dasannikov@gmail.com>
2023-09-13 12:02:27 +0200

    Initial commit

---
+++ Assets/SuperLib/Scripts/TestScript.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace SuperLib {
+
+    public class TestScript : MonoBehaviour {
+
+        void Start() {
+            Debug.Log("Hello from the SuperLib!");
+        }
+
+    }
+    
+}
